6bb29eb33787737c6e25c9ff9fe256835996e081
shahilpatwary <74975968+shahilpatwary@users.noreply.github.com>
2021-03-14 11:20:53 +0600

    3nd

--- MID_LAB_EXAM/controller/regcheck.php
+++ MID_LAB_EXAM/controller/regcheck.php
@@ -2,54 +2,35 @@
 	session_start();
 
     if (isset($_POST['submit']))
-    {   
-        $Id=$_POST['id']    
-        $name = $_POST['username'];
+    {        
+        $id = $_POST['id'];
+        $username = $_POST['username'];
         $password  = $_POST['password'];
         $re_password = $_POST['re_password'];
         $usertype = $_POST['usertype'];
+  
         
-        if ($Id == "" || $name =="" || $password == "") 
+        if ($id == "" || $username =="" || $password == "") 
         { 
             echo "Enter the all value";
         } 
         else{
             if($password==$re_password)
             {
-                $customer = ['name'=> $name, 'password'=> $password, 'Id'=>$Id,'usertype'=>$usertype];
-				$_SESSION['current_user'] = $customer;
+                $user = ['username'=> $username, 'password'=> $password, 'id'=>$id,'usertype'=>$usertype];
+				$_SESSION['current_user'] = $user;
                 
                 $file_location='../model/user.json';
 
 				if(filesize($file_location)==0){
                     $myfile=fopen($file_location, 'w');
-                    $json=json_encode($customer);
+                    $json=json_encode($user);
                     fwrite($myfile, '['.$json.']');
 				}
 				else{
-                    $customers = json_decode(file_get_contents($file_location),true);
-                    $flag = false;
-                    if(isset($_SESSION['loggedInUser'])){
-                        $loggedInUser = $_SESSION['loggedInUser'];
-                    }
-                    
-                    foreach ($customers as $user => $entry){ 
-                        if($entry['Id'] == $loggedInUser['Id']){
-                            $customer[$user]['name'] == $customer['name'];
-                            $customer[$user]['password'] == $customer['password'];
-                            $_SESSION['loggedInUser'] = $customer;
-                            $flag = true;
-                            break;
-                        }
-                    }
-                    if($flag){ 
-						file_put_contents($file_location, json_encode($customers));
-                    }
-                    else{
-                
-						array_push($customers, $customer);
-						file_put_contents($file_location, json_encode($customers));
-                    }
+						$prevUsers = json_decode(file_get_contents($file_location));
+						array_push($prevUsers, $user);
+						file_put_contents($file_location, json_encode($prevUsers));
 				}
 				header('location: ../view/login.html');
             }
